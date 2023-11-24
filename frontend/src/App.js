185a7ab37edf56ca6ad69a0fd325d4e6b2ba8247
Opod <48951218+DylanL0ng@users.noreply.github.com>
2023-11-24 01:41:24 +0000

    fix: address minor bugs in frontend and backend components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,6 @@ function App() {
 
     setFetchingData(REQUEST_STATES.waiting_request);
     setErrorMessage(undefined);
-
     try {
       const response = await fetch(`http://${process.env.REACT_APP_EXPRESS_IP}:${process.env.REACT_APP_EXPRESS_PORT}/countries/${search_input}`);
       if (!response.ok)
@@ -33,7 +32,7 @@ function App() {
         setErrorMessage("Sorry, no country could be found. Please try again.")
         return
       }
-      
+            
       const data = await response.json();
       
       setCountriesData(data);
