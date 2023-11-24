af797ea0085d9ba5b7ba5f8d0cb795e86ee0720e
Opod <48951218+DylanL0ng@users.noreply.github.com>
2023-11-24 01:16:21 +0000

    test: finalise testing for backend route

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+// Request modules
+import express from 'express'
+import axios from 'axios'
+import cors from "cors";
+
+const app = express();
+
+// Setup and use CORS
+app.use(cors());
+
+app.get('/countries/:country', async (req, res) => {
+    let { country } = req.params;
+    
+    // Remove any whitespace from the param
+    country = country.trim()
+
+    // If param isn't passed through or its passed through
+    // with invalid symbols, such as numbers then cancel out
+    // and respond with an error message.
+    if (!country || !/^[a-zA-Z]+$/.test(country))
+        return res.status(400).json({ error: "Invalid or missing country parameter. Please provide a valid country name." })
+
+    // Attempt to query for the country with the API
+    try {
+        // Encode the param as a valid URI component
+        // This will prevent and possible injection attacks.
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${encodeURIComponent(country)}`)
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data', error.message);
+        res.status(500).json({ error: `Failed to fetch data: ${error.message}` });
+    }
+})
+
+export default app
--- backend/app.test.js
+++ backend/app.test.js
@@ -1,6 +1,5 @@
-import supertest from "supertest";
-import { request } from "express";
-
+import app from "./app";
+import request from 'supertest'
 
 describe("GET /countries", () => {
     
@@ -10,26 +9,47 @@ describe("GET /countries", () => {
         // the given country
 
         // should respond with a 200 status code
-        test("should respond with a 200 status code", async () => {
-            const response = await request(app).get("/countries").send({
-                country: "Ireland"
-            })
 
+        test("should respond with a 200 status code", async () => {
+            const response = await request(app).get(`/countries/Ireland`)
             expect(response.statusCode).toBe(200)
+            expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        })
+    }) 
+    
+    describe("when the country name param is missing", () => {
+        // should send the get request and run
+        // into issues as no country has been
+        // given
+
+        // should respond with status code of 404
+        test("should respond with a 404 status code", async () => {
+            const response = await request(app).get(`/countries/`)
+            expect(response.statusCode).toBe(404)    
         })
     }) 
 
-    describe("when the country name param is missing", () => {
-        // should respond with status code of 400
-
-        // should respond with error mmessage prompting
-        // the reason behind the error
-    }) 
-
     describe("given an invalid country name", () => {
+        // should send the get request and
+        // run into issues as invalid characters
+        // were entered
+
+        // should respond with status code of 400
+        test("should respond with a 400 status code", async () => {
+            const response = await request(app).get(`/countries/1reland`)
+            expect(response.statusCode).toBe(400)
+        })
 
     }) 
     describe("given an unknown country name", () => {
+        // should send the get request and
+        // run into issues as the country name
+        // is not known
 
+        // should respond with status code of 500
+        test("should respond with a 500 status code", async () => {
+            const response = await request(app).get(`/countries/RandomCountry`)
+            expect(response.statusCode).toBe(500)
+        })
     }) 
 })
